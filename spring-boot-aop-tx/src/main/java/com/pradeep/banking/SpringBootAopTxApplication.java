caee613d7608ab84c850775b97b188adaafb2b9e
Rahul <rahulcharvekar@gmail.com>
2020-02-29 14:39:41 +0530

    First Commit

---
+++ spring-boot-aop-tx/src/main/java/com/pradeep/banking/SpringBootAopTxApplication.java
@@ -0,0 +1,13 @@
+package com.pradeep.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopTxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopTxApplication.class, args);
+	}
+
+}
---
+++ spring-boot-aop-tx/src/test/java/com/pradeep/cms/SpringBootAopTxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAopTxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
